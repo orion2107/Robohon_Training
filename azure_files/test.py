944ed8b22633d694e80f11917d767751d641a8fa
yonigotliv <orion2107@gmail.com>
2022-11-25 15:38:13 +0200

    Reading default list from csv file

--- azure_files/test.py
+++ azure_files/test.py
@@ -4,8 +4,6 @@ sentence = 'תגידי לי איזו ביצועים נתמכות על ידך?'
 threshold = 0.6
 input_data={"data":[sentence, threshold]}
 
-
-
 scoring_uri = "http://20.62.153.81:80/api/v1/service/alephbert-deployed/score"
 prediction = requests.post(scoring_uri, json = input_data)
 
